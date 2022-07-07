941533da86ca1b218a8e4be97dd9103d420be6b4
ohsoomansour <ceoosm@naver.com>
2022-07-08 01:23:02 +0900

    update6

--- src/App.js
+++ src/App.js
@@ -110,12 +110,10 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path= "movie/:id" element={<Detail />} /> 
       </Routes>
-    </BrowserRouter> 
   )
    
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
+
 /* import React from 'react';
+import ReactDOM from 'react-dom/';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+); */
+
+
+
+
+
+
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,18 +27,9 @@ root.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-); */
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-      <App />    
-  </React.StrictMode>,
-  document.getElementById('root')
 );
+
+
+
 
 
